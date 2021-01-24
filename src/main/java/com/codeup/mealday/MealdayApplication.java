826901bfa7f410997b3c15fd0b38261e4f5483c6
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2021-01-24 17:18:35 -0600

    first commit

---
+++ src/main/java/com/codeup/mealday/MealdayApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.mealday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealdayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealdayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/mealday/MealdayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.mealday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealdayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
