d108251dc634a44285f0413273f881f953a3259c
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2021-01-27 22:19:43 -0600

    added profile page and controller

---
+++ src/main/java/com/codeup/mealday/controllers/ProfileController.java
@@ -0,0 +1,13 @@
+package com.codeup.mealday.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProfileController {
+
+    @GetMapping("/profile/")
+    public String indexPage(){
+        return "profile";
+    }
+}
