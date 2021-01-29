ba81ebe94df38663a2d9bbc622dc3ab4ef56657d
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2021-01-28 22:39:57 -0600

    MVC connected for profile

--- src/main/java/com/codeup/mealday/controllers/ProfileController.java
+++ src/main/java/com/codeup/mealday/controllers/ProfileController.java
@@ -1,13 +1,24 @@
 package com.codeup.mealday.controllers;
 
+import com.codeup.mealday.models.User;
+import com.codeup.mealday.repos.UserRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ProfileController {
+    private final UserRepository userDao;
 
-    @GetMapping("/profile/")
-    public String indexPage(){
+    public ProfileController(UserRepository userDao){
+        this.userDao = userDao;
+    }
+
+    @GetMapping("/profile/{id}")
+    public String indexPage(@PathVariable int id, Model model){
+        User user = userDao.findById(1);
+        model.addAttribute("user", user.getUsername());
         return "profile";
     }
 }
